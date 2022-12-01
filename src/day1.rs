26f5989909717b20266788252e2c041e99d214c8
ashermkn1 <ashkornfeld@gmail.com>
2022-12-01 18:02:48 -0500

    reformat and shrink solution

--- src/day1.rs
+++ src/day1.rs
@@ -2,25 +2,14 @@
 pub fn day1_input(input: &str) -> Vec<Vec<u32>> {
     input
         .split("\n\n")
-        .map(|l| {
-            l.lines().map(|m| {
-                m.parse::<u32>().unwrap()
-            }).collect()
-        }).collect()
+        .map(|l| l.lines().map(|m| m.parse::<u32>().unwrap()).collect())
+        .collect()
 }
 #[aoc(day1, part1)]
 pub fn part1(input: &[Vec<u32>]) -> u32 {
-    let mut elfmax: u32 = 0;
-    for elf in input {
-
-        let s = elf.iter().sum();
-        if s > elfmax {
-            elfmax = s;
-        }
-    }
-    elfmax
+    input.iter().map(|elf| elf.iter().sum()).max().unwrap()
 }
-#[aoc(day1,part2)]
+#[aoc(day1, part2)]
 pub fn part2(input: &[Vec<u32>]) -> u32 {
     let mut calorie_counts: Vec<u32> = input.iter().map(|elf| elf.iter().sum()).collect();
     calorie_counts.sort();
